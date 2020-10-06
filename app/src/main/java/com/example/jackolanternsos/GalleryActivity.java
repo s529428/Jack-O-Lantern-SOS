9ae67f2b495838b3faefe25e0ec5982c3e928c33
Lindsay Gillespie <S529464@nwmissouri.edu>
2020-10-06 16:54:35 -0500

    worked on transition

--- app/src/main/java/com/example/jackolanternsos/GalleryActivity.java
+++ app/src/main/java/com/example/jackolanternsos/GalleryActivity.java
@@ -2,7 +2,11 @@ package com.example.jackolanternsos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GalleryActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class GalleryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+        Button generateBTN= findViewById(R.id.GenerateBTN);
+        generateBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //switching to main Actvity
+                finish();
+            }
+        });
     }
+
 }
