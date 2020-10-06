5cf6ee08a673a9594a4b4bcc4583002965c30292
Caroline Finnerty <caroline@finnerty.org>
2020-10-06 16:46:07 -0500

    Gallery Button

--- app/src/main/java/com/example/jackolanternsos/MainActivity.java
+++ app/src/main/java/com/example/jackolanternsos/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.jackolanternsos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 //hello Group Mates
@@ -13,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("hello world", "jello");
+    }
+
+    public void goToGallery(View v){
+        Intent goGallery = new Intent(this, GalleryActivity.class);
+        startActivityForResult(goGallery, 1);
     }
 }
